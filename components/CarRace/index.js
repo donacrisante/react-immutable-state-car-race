0f875e8e32a60aee73b27f42fa608e76432c8047
Donatella Crisante <dona.crisante@zoho.com>
2023-06-21 21:03:07 +0200

    copy challenge from code sandbox

--- components/CarRace/index.js
+++ components/CarRace/index.js
@@ -1,4 +1,5 @@
-import { Fragment, useState } from "react";
+import { useImmer } from "use-immer";
+import { Fragment } from "react";
 import Winner from "../Winner";
 import {
   CarButton,
@@ -12,13 +13,36 @@ import { initialCars, getRandomDistance } from "../../utils/utils";
 const finishLine = 200;
 
 export default function CarRace() {
-  const [cars, setCars] = useState(initialCars);
+  /* const [cars, setCars] = useState(initialCars); */
+  const [cars, updateCars] = useImmer(initialCars);
 
   function moveCar(clickedCar) {
     const coveredDistance = getRandomDistance();
     console.log("clickedCar", clickedCar);
     console.log("coveredDistance", coveredDistance);
+
+    updateCars((draft) => {
+      const selectedCar = draft.find(({ emoji }) => emoji === clickedCar.emoji);
+      selectedCar.position.x += coveredDistance;
+      selectedCar.position.lastDistance = coveredDistance;
+    });
   }
+
+  /* Spread syntax:
+    setCars(
+      cars.map((car) =>
+        car.emoji === clickedCar.emoji
+          ? {
+              ...car,
+              position: {
+                lastDistance: coveredDistance,
+                x: car.position.x + car.position.lastDistance,
+              },
+            }
+          : car
+      )
+    );
+  } */
 
   const winner = cars.find((car) => car.position.x >= finishLine);
 
